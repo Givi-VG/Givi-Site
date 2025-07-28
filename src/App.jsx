1067f7a9128000fa743ff64b123df55dcd990525
Givi-VG <givi.dev2000@gmail.com>
2025-07-28 21:02:43 +0530

    Update App.jsx
    Added the SpeedInsights import things

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Routes from "./Routes";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
   return (
-    <Routes />
+    <>
+      <Routes />
+      <SpeedInsights />
+    </>
   );
 }
 
